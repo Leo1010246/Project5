4f0f3d81b4dab23e28f6c101d5c6c88b39e3579e
YujinCho <22100708@handong.ac.kr>
2023-12-09 14:56:34 +0900

    Try to fix Login error

--- src/main/java/com/example/user/LoginController.java
+++ src/main/java/com/example/user/LoginController.java
@@ -19,20 +19,22 @@ public class LoginController {
     }
 
     // 로그인 하는 부분
-    @RequestMapping(value = "/loginOK", method = RequestMethod.POST)
+    @RequestMapping(value = "/loginOk", method = RequestMethod.POST)
     public String loginCheck(HttpSession session, UserVO vo){
+        System.out.println("~로그인 시도~ try to login");
         String returnURL = "";
         if (session.getAttribute("login") != null) {
             session.removeAttribute("login");
+            System.out.println("로그인 정보 입력받음");
         }
 
         UserVO loginvo = service.getUser(vo);
         if(loginvo != null){
-            System.out.println("로그인 성공");
+            System.out.println("로그인 성공 login success");
             session.setAttribute("login", loginvo);
             returnURL = "redirect:/board/list";
         } else {
-            System.out.println("로그인 실패!");
+            System.out.println("로그인 실패! login failed");
             returnURL = "redirect:/login/login";
         }
         return returnURL;
